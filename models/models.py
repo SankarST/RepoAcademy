4aba4297a47f3992314a40234521e401336bbd9d
sankar <sankar.sthillai@gmail.com>
2021-03-08 10:44:58 +0530

    Editing in Odoo admin - Models, Access, Views for Teachers , Courses , relations between models

--- models/models.py
+++ models/models.py
@@ -4,9 +4,20 @@ from odoo import models, fields, api
 
 class Teachers(models.Model):
 	_name = 'academy.teachers'
-	
+
 	name = fields.Char()
 	biography = fields.Html()
+
+	course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+
+#        course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
 
 
 class academy(models.Model):
