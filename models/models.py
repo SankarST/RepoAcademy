f9831799e7af69381ac0e69f3a445d0a1948866d
sankar <sankar.sthillai@gmail.com>
2021-04-08 19:07:50 +0530

    Code updated to add e-commerce categories , mapping the courses to it. Inheriting templates to customize display of products

--- models/models.py
+++ models/models.py
@@ -3,27 +3,22 @@
 from odoo import models, fields, api
 
 class Teachers(models.Model):
-	_name = 'academy.teachers'
+        _name = 'academy.teachers'
 
-	name = fields.Char()
-	biography = fields.Html()
+        name = fields.Char()
+        biography = fields.Html()
 
-	course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
-
-
+        course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
 #        course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
 
 class Courses(models.Model):
-    _name = 'academy.courses'
-    _inherit = 'mail.thread'
+#       _name = 'academy.courses'
+#        _inherit = 'mail.thread'
+        _inherit = 'product.template'
 
-    name = fields.Char()
-    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
+        name = fields.Char()
+        teacher_id = fields.Many2one('academy.teachers', string="Teacher")
 
-
-class academy(models.Model):
-       _name = 'academy.academy'
-       _description = 'academy.academy'
 
 #     name = fields.Char()
 #     value = fields.Integer()
